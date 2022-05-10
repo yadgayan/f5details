93c902f1dda29ecf354671b38340417d2f1282ae
yadgayan <103494726+yadgayan@users.noreply.github.com>
2022-05-10 08:45:32 +0000

    Update makezip.py

--- makezip.py
+++ makezip.py
@@ -8,12 +8,12 @@ def main():
 # Check if file exists
 	if path.exists("/tmp/F5_details.txt"):
 # get the path to the file in the current directory
-	src = path.realpath("");
+	src = path.realpath("F5_details.txt");
 # rename the original file
 	os.rename("career.guru99.txt","guru99.txt")
 # now put things into a ZIP archive
 	root_dir,tail = path.split(src)
-    shutil.make_archive("guru99 archive", "zip", root_dir)
+        shutil.make_archive("guru99 archive", "zip", root_dir)
 # more fine-grained control over ZIP files
 	with ZipFile("testguru99.zip","w") as newzip:
 	newzip.write("F5_details.txt")
