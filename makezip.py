74cbca284357b30a77ab88b1b8b317cbc9d8c336
yadgayan <103494726+yadgayan@users.noreply.github.com>
2022-05-10 09:01:31 +0000

    Update makezip.py

--- makezip.py
+++ makezip.py
@@ -1,18 +1,45 @@
-
-import os
-import shutil
 from zipfile import ZipFile
-from os import path
-from shutil import make_archive
-
-
-    # get the path to the file in the current directory
-        src = path.realpath("/tmp/F5_details.txt");
- 
-    # now put things into a ZIP archive
-        root_dir,tail = path.split(src)
-        shutil.make_archive("guru99 archive","zip",root_dir)
-    # more fine-grained control over ZIP files
-        with ZipFile("testguru99.zip", "w") as newzip:
-            newzip.write("F5_details.txt")
-            newzip.write("F5_details.txt.bak")
+import os
+from os.path import basename
+# Zip the files from given directory that matches the filter
+def zipFilesInDir(dirName, zipFileName, filter):
+   # create a ZipFile object
+   with ZipFile(zipFileName, 'w') as zipObj:
+       # Iterate over all the files in directory
+       for folderName, subfolders, filenames in os.walk(dirName):
+           for filename in filenames:
+               if filter(filename):
+                   # create complete filepath of file in directory
+                   filePath = os.path.join(folderName, filename)
+                   # Add file to zip
+                   zipObj.write(filePath, basename(filePath))
+def main():
+    print('*** Create a zip file from multiple files  ')
+    #create a ZipFile object
+    zipObj = ZipFile('sample.zip', 'w')
+    # Add multiple files to the zip
+    zipObj.write('/tmp/F5_details.txt')
+    
+    # close the Zip File
+    zipObj.close()
+    print('*** Create a zip file from multiple files using with ')
+    # Create a ZipFile Object
+    with ZipFile('sample2.zip', 'w') as zipObj2:
+       # Add multiple files to the zip
+       zipObj2.write('/tmp/F5_details.txt')
+   
+    # Name of the Directory to be zipped
+    dirName = 'sampleDir'
+    # create a ZipFile object
+    with ZipFile('sampleDir.zip', 'w') as zipObj:
+       # Iterate over all the files in directory
+       for folderName, subfolders, filenames in os.walk(dirName):
+           for filename in filenames:
+               #create complete filepath of file in directory
+               filePath = os.path.join(folderName, filename)
+               # Add file to zip
+               zipObj.write(filePath)
+    print('*** Create a zip archive of only csv files form a directory ***')
+    zipFilesInDir('sampleDir', 'sampleDir2.zip', lambda name : 'csv' in name)
+if __name__ == '__main__':
+   main()
