2918bc1e99f694fbe80c051846a52f35dd7ec55d
yadgayan <103494726+yadgayan@users.noreply.github.com>
2022-05-10 08:21:39 +0000

    Create makezip.py

---
+++ makezip.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+from zipfile import ZipFile
+from os import path
+from shutil import make_archive
+
+def main():
+# Check if file exists
+	if path.exists("/tmp/F5_details.txt"):
+# get the path to the file in the current directory
+	src = path.realpath("");
+# rename the original file
+	os.rename("career.guru99.txt","guru99.txt")
+# now put things into a ZIP archive
+	root_dir,tail = path.split(src)
+    shutil.make_archive("guru99 archive", "zip", root_dir)
+# more fine-grained control over ZIP files
+	with ZipFile("testguru99.zip","w") as newzip:
+	newzip.write("F5_details.txt")
+	    newzip.write("F5_details.txt.bak")
+if __name__== "__main__":
+	  main()
