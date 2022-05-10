938353e3529ce4a1124681c9c72dee31a364a382
yadgayan <103494726+yadgayan@users.noreply.github.com>
2022-05-10 09:06:41 +0000

    Update makezip.py

--- makezip.py
+++ makezip.py
@@ -22,24 +22,8 @@ def main():
     
     # close the Zip File
     zipObj.close()
-    print('*** Create a zip file from multiple files using with ')
-    # Create a ZipFile Object
-    with ZipFile('sample2.zip', 'w') as zipObj2:
-       # Add multiple files to the zip
-       zipObj2.write('/tmp/F5_details.txt')
+ 
    
-    # Name of the Directory to be zipped
-    dirName = 'sampleDir'
-    # create a ZipFile object
-    with ZipFile('sampleDir.zip', 'w') as zipObj:
-       # Iterate over all the files in directory
-       for folderName, subfolders, filenames in os.walk(dirName):
-           for filename in filenames:
-               #create complete filepath of file in directory
-               filePath = os.path.join(folderName, filename)
-               # Add file to zip
-               zipObj.write(filePath)
-    print('*** Create a zip archive of only csv files form a directory ***')
-    zipFilesInDir('sampleDir', 'sampleDir2.zip', lambda name : 'csv' in name)
+ 
 if __name__ == '__main__':
    main()
